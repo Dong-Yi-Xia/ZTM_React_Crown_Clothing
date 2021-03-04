7216f4b9161da30d3fb98e8f71cbb17dcc41df19
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-03-04 11:44:42 -0500

    homepage into smaller component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,11 +5,10 @@ import './menu-item.styles.scss'
 // Destructing the props
 const MenuItem = ( {title, imageUrl, size} ) => (
     // inline style to dynamically change the image, Use backgroundColor instead of background-color:
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }} className={`${size} menu-item`} >
+    <div className={`${size} menu-item`} >
+        <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} /> 
         <div className='content'>
-            <h1 className='title'>{title}</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
