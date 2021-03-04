b2e09623025c11083e2d349f63248ecf7ec47eb6
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-03-03 21:03:22 -0500

    menu styling

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 import './menu-item.styles.scss'
 
 // Destructing the props
-const MenuItem = ( {title, imageUrl} ) => (
+const MenuItem = ( {title, imageUrl, size} ) => (
     // inline style to dynamically change the image, Use backgroundColor instead of background-color:
     <div style={{
         backgroundImage: `url(${imageUrl})`
-    }} className='menu-item'>
+    }} className={`${size} menu-item`} >
         <div className='content'>
             <h1 className='title'>{title}</h1>
             <span className='subtitle'>SHOP NOW</span>
