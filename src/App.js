ce7cb251535fe59495121c962addd4e73833be15
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-03-08 21:24:51 -0500

    firebase signin signout

--- src/App.js
+++ src/App.js
@@ -9,20 +9,49 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 
 import Header from './components/header/header.component'
 
+import { auth } from './firebase/firebase.utils'
 
-function App() {
-  return (
-    <div>
-      <Header/>
+class App extends React.Component {
+  constructor(){
+    super()
 
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
+    this.state = {
+      currentUser: null
+    }
+  }
 
-    </div>
-  );
+  unsubscribeFromAuth = null
+
+  componentDidMount(){
+    //a firebase auth method, onAuthStateChanged method, open subscription 
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({
+        currentUser: user
+      })
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount(){
+    //to close the subscription, prevent memory leak
+    this.unsubscribeFromAuth()
+  }
+
+
+  render(){
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+
+      </div>
+    )
+  }
 }
 
 export default App;
